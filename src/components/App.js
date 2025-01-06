09f2d3dc1cd50d07c2dfe2af819c8207a3233d8b
accio-rajitha <rajithasabhavath2019@gmail.com>
2025-01-06 12:53:16 +0530

    code1

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,35 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
+
+function App() {
+  // State to manage whether the paragraph is visible or not
+  const [isParagraphVisible, setIsParagraphVisible] = useState(false);
+
+  // Function to handle button click
+  const handleClick = () => {
+    setIsParagraphVisible(true); // Set paragraph visibility to true when clicked
+  };
+
+  return (
+    <div id="main">
+      {/* Button to trigger the paragraph rendering */}
+      <button id="click" onClick={handleClick}>
+        Click me
+      </button>
+
+      {/* Conditional rendering of paragraph */}
+      {isParagraphVisible && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default App;
+/*
+
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
@@ -11,3 +43,4 @@ function App() {
 
 
 export default App;
+*/
